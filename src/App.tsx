1cb41ae412d4848173c2ede13fcafe209a93009d
wonderLuc <workmaildo@yandex.ru>
2021-09-09 22:37:48 +0500

    feat: add router and routes to App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './style.scss';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import HelloPage from './hello-page/hello-page';
+import NotFound from './404/404';
+import Header from './header/header';
+import GamePage from './game-page/game-page';
 
 const App = (): JSX.Element => {
   return (
-    <div></div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <HelloPage />
+          </Route>
+          <Route path="/lobby"><NotFound /></Route>
+          <Route path="/game"><GamePage /></Route>
+          <Route path="*"><NotFound /></Route>
+        </Switch>        
+      </Router>
+    </>
   );
 };
 
