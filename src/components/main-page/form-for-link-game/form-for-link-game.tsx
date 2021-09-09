82a3b5961f13010b2e88f843192e0c459d3270fd
vladislavho <introwlad@gmail.com>
2021-09-09 23:00:06 +0300

    feat: added a component form to go to the game page

---
+++ src/components/main-page/form-for-link-game/form-for-link-game.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import './style/from-for-link-game.sass';
+
+
+export const FromForLinkGame = ():ReactElement =>{
+  const pageGame = 'https/qwerty/game/#id1';
+
+
+
+  const [textValue, setTextValue] = useState<string>('');
+  const [validButton, setValidButton] = useState<boolean>(false);
+
+  useEffect(()=>{
+    if (textValue.length){
+      setValidButton(false);
+    } else {
+      setValidButton(true);
+    }
+  }, [textValue]);
+
+
+  function handleValue(e:React.ChangeEvent<HTMLInputElement>) {
+    setTextValue(e.target.value);
+  }
+ 
+  function handleClick(e:React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    if (textValue === pageGame) {
+      console.log(true);
+      setTextValue('');
+    } else {
+      console.log(false);
+    }
+  }
+
+  return (
+    <div className="App">
+      <p>Connect to lobby by URL: </p>
+      <form>
+        <input type="text" value={textValue} onChange={(e)=> handleValue(e)}/>
+        <button disabled ={validButton} onClick={(e) => handleClick(e)}>Play</button>
+      </form>
+    </div>
+  );
+};
---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+import { FromForLinkGame } from '../../components/main-page/form-for-link-game/form-for-link-game';
+import './style/main-page.sass';
+
+export const MainPage = ():ReactElement =>{
+  return (
+    <FromForLinkGame/>
+  );
+};
