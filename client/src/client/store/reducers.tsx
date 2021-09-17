485d8db36224825007b627552622ce285c4ce47f
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-09-17 20:57:44 +0300

    refactor: rename variable

--- client/src/client/store/reducers.tsx
+++ client/src/client/store/reducers.tsx
@@ -3,14 +3,14 @@ import {
 } from '../types/actions-types';
 import { ISettings, IStore, IUserInfo } from '../types/store-types';
 
-const mockUser: IUserInfo = { name: 'Alex' };
-const mockSettings: ISettings = { time: 30 };
-const mockStore: IStore = {
-  user: mockUser,
-  settings: mockSettings,
+const user: IUserInfo = { name: 'Alex' };
+const settings: ISettings = { time: 30 };
+const initialStore: IStore = {
+  user: user,
+  settings: settings,
 };
 
-export default function reducer(state: IStore = mockStore, action: AllActions) {
+export default function reducer(state: IStore = initialStore, action: AllActions) {
   switch (action.type) {
     case Actions.UPDATE_USERINFO:
       return { ...state, user: { ...action.payload } };
