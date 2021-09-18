6616afe6f5ebb6726ec5ac0b896eb79492b017bc
wonderLuc <workmaildo@yandex.ru>
2021-09-18 14:47:35 +0500

    feat: add possibility to copy link on dealer card

--- client/src/client/components/lobby-d-actions/lobby-d-actions.tsx
+++ client/src/client/components/lobby-d-actions/lobby-d-actions.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './style.scss';
 
 export default function LobbyDealerActions(): JSX.Element {
+  const linkText = 'example_link';
+
+  function handleCopy(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    navigator.clipboard.writeText(linkText);
+
+  }
   return (
     <section className="dealer-actions">
       <h3 className="dealer-actions__link-title">Link To Lobby:</h3>
       <div className="dealer-actions__link-elements">
-        <input className="dealer-actions__link" type="text" placeholder="there will be a link" />
-        <button className="btn btn_active dealer-actions__button" type="button">Copy Link</button>
+        <input className="dealer-actions__link" type="text" value={linkText} readOnly/>
+        <button className="btn btn_active dealer-actions__button" type="button"
+          onClick={handleCopy}
+        >Copy Link</button>
       </div>
       <div className="dealer-actions__game">
         <button className="btn btn_active dealer-actions__game-start" type="button">Start Game</button>
