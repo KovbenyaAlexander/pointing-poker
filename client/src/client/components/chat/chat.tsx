b189b1c4cbb165f03e43581e59fe3848418923b0
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-09-22 21:38:13 +0300

    feat: add simple styles for chat

--- client/src/client/components/chat/chat.tsx
+++ client/src/client/components/chat/chat.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { IStore } from '../../types';
+import './style.scss';
 
 const Chat = () => {
   const [message, setMessage] = useState('');
@@ -12,10 +13,13 @@ const Chat = () => {
   }));
 
   useEffect(() => {
-    socket.on('message', (data: any) => {
-      console.log(data);
-      setMessages((prev: any) => [...prev, data]);
-    });
+    const socketUpdate = async ()=>{
+      await socket.on('message', (data: any) => {
+        console.log(data);
+        setMessages((prev: any) => [...prev, data]);
+      });
+    }
+    socketUpdate()
   }, [socket]);
 
   const sendMessage = () => {
@@ -30,19 +34,30 @@ const Chat = () => {
       />
       <button type="button" onClick={sendMessage}>Send</button>
       <hr />
-      {messages.map((msg: any) => (
-        <div key={Math.random()}>
-          <span>
-            Author:
-            {msg.name}
-          </span>
-          <span>
-            Message:
-            {msg.message}
-          </span>
-          <br />
-        </div>
-      ))}
+      <div className='messages'>
+      {messages.map((msg: any, i: number) => {
+        let classes;
+        if (name === msg.name) {
+          classes = 'messages__msg-left';
+        } else {
+          classes = 'messages__msg-right';
+        }
+
+        return (
+          <div key={i} className={classes}>
+            <span>
+              Author:
+              {msg.name}
+            </span>
+            <span>
+              Message:
+              {msg.message}
+            </span>
+          </div>
+        );
+      })}
+      </div>
+
     </div>
   );
 };
